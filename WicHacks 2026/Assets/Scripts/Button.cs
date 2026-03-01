9947a78b592fce0642dd3493558698021b953507
sz5804 <sz5804@rit.edu>
2026-03-01 00:19:00 -0500

    design select fixed

--- WicHacks 2026/Assets/Scripts/Button.cs
+++ WicHacks 2026/Assets/Scripts/Button.cs
@@ -13,10 +13,34 @@ public class Button : MonoBehaviour
     public void OnClick()
     {
         // toggles active state of menus
-        activate.SetActive(true);
         GameManager.Instance.activeMenu.SetActive(false);
+        // update values
         GameManager.Instance.activeMenu = activate;
+        if (GameManager.Instance.activeCategory != category)
+            GameManager.Instance.updateCategory = true;
         GameManager.Instance.activeCategory = category;
+        switch ((int)category)
+        {
+            case 0:
+                GameManager.Instance.activeSpriteList = SpriteLibraryManager.Instance.hairSprites;
+                break;
+            case 1:
+                GameManager.Instance.activeSpriteList = SpriteLibraryManager.Instance.dressSprites;
+                break;
+            case 2:
+                GameManager.Instance.activeSpriteList = SpriteLibraryManager.Instance.topSprites;
+                break;
+            case 3:
+                GameManager.Instance.activeSpriteList = SpriteLibraryManager.Instance.bottomSprites;
+                break;
+            case 4:
+                GameManager.Instance.activeSpriteList = SpriteLibraryManager.Instance.shoeSprites;
+                break;
+            case 5:
+                break;
+        }
+        // activate new menu
+        activate.SetActive(true);
     }
 
     public void SelectClothes()
